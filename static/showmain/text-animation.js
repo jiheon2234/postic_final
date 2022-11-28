92758680288dc4d4e0485b3fb35bcfb300859066
jiheon Kim <jiheon2234@gmail.com>
2022-11-28 18:52:41 +0900

    real_real_real_real_real_final

--- static/showmain/text-animation.js
+++ static/showmain/text-animation.js
@@ -1,4 +1,5 @@
-const {children: titles} = document.querySelector(".animate-text");
+// 채용공고 키워드
+const { children: titles } = document.querySelector(".animate-text");
 const txtsLen = titles.length;
 let index = 0;
 const textInTimer = 3000;
@@ -25,3 +26,32 @@ function animateText() {
 }
 
 window.onload = animateText;
+
+// 총 채용건수
+let number = document.getElementById("number");
+
+let start = 20;
+let end = 61782;
+let ticks = 40;
+let speed = 50;
+
+let randomNumbers = [end];
+
+for (let i = 0; i < ticks - 1; i++) {
+  randomNumbers.unshift(Math.floor(Math.random() * (end - start + 1) + start));
+}
+
+randomNumbers.sort((a, b) => {
+  return a - b;
+});
+
+console.log(randomNumbers.length);
+
+let x = 0;
+let interval = setInterval(function () {
+  number.innerHTML = randomNumbers.shift();
+
+  if (++x === ticks) {
+    window.clearInterval(interval);
+  }
+}, speed);
